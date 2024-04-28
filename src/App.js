517cb7e048ee287927f3a17144e83da84eb9e08b
Amit Saini <amitssaini999@gmail.com>
2024-04-29 02:42:46 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const customDictionary = {
+    teh: "the",
+    wrok: "work",
+    fot: "for",
+    exampl: "example",
+  };
+  const [suggestedText, setSuggestedText] = useState("");
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const keys = Object.keys(customDictionary);
+  const values = Object.values(customDictionary);
+  useEffect(() => {
+    let s = new Set();
+    inputText.split(" ").forEach((key) => s.add(key.toLowerCase()));
+    let flag = false;
+    keys.forEach((item, index) => {
+      if (s.has(item)) {
+        setSuggestedText(values[index]);
+        flag = true;
+      }
+    });
+    if (!flag) setSuggestedText("");
+  }, [inputText, keys, values]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea
+        value={inputText}
+        onChange={(e) => handleInputChange(e)}
+        placeholder="Enter text..."
+        rows={5}
+        cols={40}
+      />
+      {suggestedText && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Did you mean: <strong>{suggestedText}</strong>?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
